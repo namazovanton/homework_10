06f44a480f8c847321f0cd19797e2ab9e15661a0
namazovanton <namazovanton@gmail.com>
2022-10-09 21:25:04 +0300

    Fixed bugs in accordance with the requirements of PEP 8

--- workspace/utils.py
+++ workspace/utils.py
@@ -13,13 +13,13 @@ def load_candidates():
 
 def get_all():
     """
-    Открывает файл 'candidates.json'.
+    Выводит всех кандидатов.
     """
     candidates = ""
     candidates_list = load_candidates()
     for person in candidates_list:
-        candidate = "\nИмя кандидата: " + person["name"]+ \
-                    "\nПозиция кандидата: " + person["position"]+\
+        candidate = "\nИмя кандидата: " + person["name"] + \
+                    "\nПозиция кандидата: " + person["position"] +\
                     "\nНавыки кандидата: " + person["skills"]+"\n"
         candidates += candidate
     return candidates
@@ -27,14 +27,13 @@ def get_all():
 
 def get_by_pk(pk):
     """
-    Открывает файл 'candidates.json'.
-    Возвращает словарь с данными о конкретном кандидате.
+    Возвращает данные о конкретном кандидате по номеру.
     """
     candidates_list = load_candidates()
     for person in candidates_list:
         if person["pk"] == pk:
-            candidate_loaded = "\nИмя кандидата: " + person["name"]+ \
-                    "\nПозиция кандидата: " + person["position"]+\
+            candidate_loaded = "\nИмя кандидата: " + person["name"] + \
+                    "\nПозиция кандидата: " + person["position"] +\
                     "\nНавыки кандидата: " + person["skills"]+"\n"
             url_of_photo = {person["picture"]}
             return url_of_photo, candidate_loaded
@@ -44,18 +43,14 @@ def get_by_pk(pk):
 
 def get_by_skill(skill):
     """
-    Открывает файл 'candidates.json'.
-    Возвращает словарь с данными о конкретном кандидате.
+    Ищет совпадения по навыкам у кандидатов.
+    Возвращает тех кандидатов, у которых совпадения есть.
     """
     candidates = ""
     candidates_list = load_candidates()
     for person in candidates_list:
         if skill.lower() in person["skills"].lower():
-            candidates += "\nИмя кандидата: " + person["name"]+ \
-                    "\nПозиция кандидата: " + person["position"]+\
+            candidates += "\nИмя кандидата: " + person["name"] + \
+                    "\nПозиция кандидата: " + person["position"] +\
                     "\nНавыки кандидата: " + person["skills"]+"\n"
     return candidates
-
-
-# abservation = get_by_skill("Python")
-# print(abservation)
